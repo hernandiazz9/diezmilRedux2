f8ed7abc3496ec51da1a7a48e3d51558c78141cc
Eva <mac@macs-MacBook-Pro.local>
2021-05-31 11:37:13 -0300

    retomando el proyecto diezmil

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,15 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDoBwRFxTu6H4MD3lUB-lIVxSgJlmkhLNo",
-    authDomain: "auth-f20a3.firebaseapp.com",
-    databaseURL: "https://auth-f20a3.firebaseio.com",
-    projectId: "auth-f20a3",
-    storageBucket: "auth-f20a3.appspot.com",
-    messagingSenderId: "682132807744",
-    appId: "1:682132807744:web:e1bf6af93c108320d5ffa5"
+  apiKey: "AIzaSyDnm6z-GkdHs4Co8MUfnJ-2THMGpTcfHzY",
+  authDomain: "diezmil-f4144.firebaseapp.com",
+  projectId: "diezmil-f4144",
+  storageBucket: "diezmil-f4144.appspot.com",
+  messagingSenderId: "157221241052",
+  appId: "1:157221241052:web:f1ea3447e86219f7a63419"
 };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   const auth = firebase.auth()
   const provider = new firebase.auth.GoogleAuthProvider();
